e0f7c50b2ed1ed7139e408024d0d3f0f9aed6ca3
sph-668 <sophiasmile48@gmail.com>
2022-02-15 00:36:38 -0500

    find the problrm...

--- labacode.c
+++ labacode.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <string.h>
+
+#define DIRMASK 100000000000000
 
 int writerLevels(int levelCount, char *buff) {
   for (int i = levelCount, position = 0; i > 0; i--, position += 3) {
@@ -25,15 +28,17 @@ int checkLevels(char *src) {
 // если возникает ошибка при получении информации (функция stat), то -1.
 int checkOnDir(char *fileName) {
   int answer = 0;
-  struct stat *stbuf;
-  if (!stat(fileName, stbuf)) {
-    if (stbuf->st_mode == 16877) {
+  struct stat stbuf;
+  printf("%d ----------\n", stat(fileName, &stbuf));
+  if (!stat(fileName, &stbuf)) {
+    printf("%s tyt %u\n", fileName, stbuf.st_mode);
+    if (stbuf.st_mode & DIRMASK == DIRMASK) {
       answer = 0;
     } else {
       answer = 1;
     }
   } else {
-    printf("stat > %d (%s)\n", stat(fileName, stbuf), fileName);
+    printf("stat > %d (%s)\n", stat(fileName, &stbuf), fileName);
     answer = -1;
   }
   return answer;
@@ -53,12 +58,6 @@ int checkDirForDir(char *dirname) {
     }
     record = readdir(chieldDir);
   }
-  /*printf("2\n");
-  if (!record) {
-    printf("END OF %s\n", dirname);
-  }
-  printf("3\n");*/
-
   rewinddir(chieldDir);
   closedir(chieldDir);
   return 0;
@@ -66,12 +65,19 @@ int checkDirForDir(char *dirname) {
 
 int main(int argc, char *argv[]) {
   /**/
-  if (!checkOnDir(argv[1])) {
-    printf("YES\n");
-    DIR *parentDir = opendir(argv[1]);
+  struct stat stbuf;
+  char str[50];
+  strcpy(str, argv[1]);
+  //printf("%d (%s)\n", stat(str, &stbuf), str);
+  //printf("%d\n", stat("1", stbuf));
+  //printf("%d!!!\n", checkOnDir(str));
+  //printf("%d  s_isdir\n", S_IFDIR);
+  if (!checkOnDir(str)) {
+    //printf("YES %d %s\n", stbuf->st_mode, str);
+    DIR *parentDir = opendir(str);
     struct dirent *record = readdir(parentDir);
     while (record) {
-      printf("%llu %s\n", record->d_ino, record->d_name);
+      printf("%lu %s\n", record->d_ino, record->d_name);
       if (!checkOnDir(record->d_name)) {
         checkDirForDir(record->d_name);
       }
@@ -80,7 +86,7 @@ int main(int argc, char *argv[]) {
     rewinddir(parentDir);
     closedir(parentDir);
   } else {
-    printf("NO %d\n", checkOnDir(argv[1]));
+    printf("NO %s\n", str);
   }
   return 0;
 }
