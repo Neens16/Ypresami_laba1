10343140de3d897c43a4199e819b38b30acfa35e
sph-668 <sophiasmile48@gmail.com>
2022-02-24 08:38:44 -0500

    fix sega

--- finder.c
+++ finder.c
@@ -36,23 +36,26 @@ int checkOnDir(char *fileName) {
 
 int checkDirForDir(char *dirname, int mod) {
   DIR *childDir = opendir(dirname);
-  struct dirent *record = readdir(childDir);
-  char str[256], check = 0;
-  while (record && !check) {
-    strcpy(str, dirname);
-    strcat(str, "/");
-    strcat(str, record->d_name);
-    if (mod && !checkOnDir(str)) {
-      check = 1;
-    } else if (!mod && record->d_name[0] != '.' && !checkOnDir(str)) {
-      if (checkDirForDir(str, 1) == 1) {
+  char check = 0;
+  if (childDir) {
+    struct dirent *record = readdir(childDir);
+    char str[256];
+    while (record && !check) {
+      strcpy(str, dirname);
+      strcat(str, "/");
+      strcat(str, record->d_name);
+      if (mod && !checkOnDir(str)) {
         check = 1;
+      } else if (!mod && record->d_name[0] != '.' && !checkOnDir(str)) {
+        if (checkDirForDir(str, 1) == 1) {
+          check = 1;
+        }
       }
+      record = readdir(childDir);
     }
-    record = readdir(childDir);
+    rewinddir(childDir);
+    closedir(childDir);
   }
-  rewinddir(childDir);
-  closedir(childDir);
   return check;
 }
 
@@ -75,8 +78,7 @@ result checker(int argc, char *argv[]) {
           strcpy(str, argv[1]);
           strcat(str, "/");
           strcat(str, record->d_name);
-          if (record->d_name[0] != '.' && !checkOnDir(str) &&
-              strcmp(record->d_name, "tmp")) {
+          if (record->d_name[0] != '.' && !checkOnDir(str)) {
             y = checkDirForDir(str, 0);
             if (y == 1) {
               strcpy(my.name[my.count], record->d_name);
